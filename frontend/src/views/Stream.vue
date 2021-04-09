ad5521d25637b3557cbf27c68eb11b6b5e43e456
Markus Bjerkhede <markus.bjerkhede@gmail.com>
2021-04-09 10:45:11 +0200

    add stream view

---
+++ frontend/src/views/Stream.vue
@@ -0,0 +1,15 @@
+
+    computed: {
+     getStreams() {
+        return this.$store.state.streams;
+      }
+    },
+    beforeMount(){
+      this.$store.dispatch('fetchStreams')
+    },
+    methods: {
+      goTo() {
+        this.$router.push('/stream')
+      }
+    }
+}
