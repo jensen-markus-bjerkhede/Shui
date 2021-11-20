4e979c931f3f1e9c5b11afe4abe53d4a0540fbe9
Markus Bjerkhede <markus.bjerkhede@gmail.com>
2021-11-20 15:35:27 +0100

    fix some style

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="Landing" class="center-flexbox">
+    <img src="@/assets/logo.svg" alt="Shiu logo">
+    <h1>SHUI</h1>
+    <h2>FLOW FREELY</h2>
     <nav>
-      <router-link to="/register">Create Account</router-link>
-      <router-link to="/login">Sign-in</router-link>
+      <router-link class="link" to="/register">Create Account</router-link>
+      <router-link class="link" to="/login">Sign-in</router-link>
     </nav>
     <waves />
   </div>
@@ -19,3 +22,55 @@ export default {
   },
 };
 </script>
+<style lang="scss">
+@import './../scss/variables';
+#landing {
+  margin: auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    margin-top: auto;
+  }
+  .padding {
+    padding: 2rem;
+  }
+  h1 {
+    font-size: 3rem;
+    color: white;
+    margin: .5rem 0;
+  }
+
+  h2 {
+    margin: 0;
+    color: $yellow;
+    font-size: 1.3rem;
+    text-transform: uppercase;
+    max-width: 16rem;
+    text-align: center;
+  }
+
+  section {
+    width: 100%;
+    margin-top: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+}
+a {
+  text-decoration: none;
+  color: white;
+  margin: 1rem
+  }
+  .link {
+    color: white;
+  }
+.footer {
+  width: 100%
+}
+</style>
